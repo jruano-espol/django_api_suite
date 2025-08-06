9e09aa2de4d481399763e5d098f1d2d911977294
jruano-espol <jruano@espol.edu.ec>
2025-08-06 19:30:33 +0000

    no se que paso

--- landing_api/views.py
+++ landing_api/views.py
@@ -15,11 +15,11 @@ class LandingAPI(APIView):
       return Response(data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        ref = db.reference(f'{self.collection_name}')
+      ref = db.reference(f'{self.collection_name}')
 
-        current_time  = datetime.now()
-        custom_format = current_time.strftime("%d/%m/%Y, %I:%M:%S %p").lower().replace('am', 'a. m.').replace('pm', 'p. m.')
-        request.data.update({"timestamp": custom_format })
+      current_time  = datetime.now()
+      custom_format = current_time.strftime("%d/%m/%Y, %I:%M:%S %p").lower().replace('am', 'a. m.').replace('pm', 'p. m.')
+      request.data.update({"timestamp": custom_format })
 
-        new_resource = ref.push(request.data)
-        return Response({"id": new_resource.key}, status=status.HTTP_201_CREATED)
+      new_resource = ref.push(request.data)
+      return Response({"id": new_resource.key}, status=status.HTTP_201_CREATED)
